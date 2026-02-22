c02569449393722978d6c3ce3f0f333e9844c589
Jitendra Kumar Gupta <149898112+jitendrakumargupta07@users.noreply.github.com>
2026-02-22 21:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (23.37%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,33 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//     if(x < 0) return false;
+//     int temp = x; 
+//     long rev = 0;
+//     while(temp > 0){
+//         rev = rev*10 + (long)temp%10;
+//         temp /= 10;
+//     }
+//     return rev == x;
+//     }
+// };
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+          if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int dup=x;
+        long long revn=0;
+        while(x>0){
+            int ld= x%10;
+            revn= ((revn*10)+ld);
+            x=x/10;
+        }
+        if (dup==revn){
+            return true;
+            }else{
+            return false;
+        }
+    }
+};
