e3e148b54702c10f8275f67b3c745261305f44e3
Jitendra Kumar Gupta <149898112+jitendrakumargupta07@users.noreply.github.com>
2026-07-09 14:31:16 +0530

    Time: 3 ms (27.08%) | Memory: 13.1 MB (81.69%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -5,17 +5,16 @@ public:
         unordered_set<int> travelDays(days.begin(), days.end());
     int lastDay = days[n - 1];
     
-    // DP array to store the minimum cost up to day i
     vector<int> dp(lastDay + 1, 0);
     
     for (int i = 1; i <= lastDay; i++) {
         if (travelDays.find(i) == travelDays.end()) {
-            dp[i] = dp[i - 1]; // No travel on this day
+            dp[i] = dp[i - 1]; 
         } else {
             dp[i] = min({
-                dp[max(0, i - 1)] + costs[0],  // 1-day pass
-                dp[max(0, i - 7)] + costs[1], // 7-day pass
-                dp[max(0, i - 30)] + costs[2] // 30-day pass
+                dp[max(0, i - 1)] + costs[0],  
+                dp[max(0, i - 7)] + costs[1], 
+                dp[max(0, i - 30)] + costs[2] 
             });
         }
     }
